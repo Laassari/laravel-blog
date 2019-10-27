98d1a960db325fee4163e83748c96bb3bde99f30
Laassari <nlaassari@gmail.com>
2019-10-27 13:30:27 +0100

    use soft delete for posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,7 +14,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = \App\Post::with('user')->get();
+        $posts = Post::with('user')->get();
         return view('posts.index', ['posts' => $posts]);
     }
 
