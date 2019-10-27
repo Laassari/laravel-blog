391eec29f1658c709475427955c0dcc0b4deeb36
Laassari <nlaassari@gmail.com>
2019-10-27 14:15:19 +0100

    hide edit and delete actions for users without permission

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -14,16 +14,18 @@
                             <a href="{{ route('posts.show', ['post' => $post->id]) }}">
                                 <h2 class="card-title d-inline">{{ $post->title }}</h2>
                             </a>
-                            <form class="d-inline" action="{{ route('posts.destroy', ['post' => $post->id]) }}" method="POST">
-                                @csrf
-                                <button type="submit" class="bg-transparent border-0 text-danger">
-                                    @method('DELETE')
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>
-                                </button>
-                            </form>
-                            <a href="{{ route('posts.edit', ['post' => $post->id]) }}">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
-                            </a>
+                           @canany(['update', 'delete'], $post)
+                                <form class="d-inline" action="{{ route('posts.destroy', ['post' => $post->id]) }}" method="POST">
+                                    @csrf
+                                    <button type="submit" class="bg-transparent border-0 text-danger">
+                                        @method('DELETE')
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>
+                                    </button>
+                                </form>
+                                <a href="{{ route('posts.edit', ['post' => $post->id]) }}">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
+                                </a>
+                           @endcanany
                     </div>
                     <p class="card-text">{{  Str::limit($post->content, 50, '...') }}</p>
                 </div>
