bf02915a542ce23a949372a7dee0ae41b3ff0a05
Laassari <nlaassari@gmail.com>
2019-12-18 10:33:59 +0100

    add post creation test

---
+++ tests/Feature/PostTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class PostTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @test post creation
+     *
+     * @return void
+     */
+
+    public function it_create_a_post_with_valid_data()
+    {
+        $user = factory('App\User')->create();
+
+        $response = $this->actingAs($user)->post(route('posts.index'), [
+            'title' => 'post title',
+            'content' => 'this is body content that should be stored',
+        ]);
+
+        $response
+            ->assertStatus(302)
+            ->assertRedirect(route('posts.index'))
+            ->assertSessionHasNoErrors();
+
+
+        $response = $this->followRedirects($response);
+
+        // should redirect to posts index
+        $response
+            ->assertStatus(200)
+            ->assertSeeText('post title')
+            ->assertSeeText('this is body');
+    }
+
+    /**
+     * @test guest should be redirected to login
+     *
+     * @return void
+     */
+
+    public function a_guest_cant_create_a_post()
+    {
+        $response = $this->post(route('posts.index'), [
+            'title' => 'post title',
+            'content' => 'this is body content that should be stored',
+        ]);
+
+        $response
+            ->assertRedirect('login');
+    }
+}
