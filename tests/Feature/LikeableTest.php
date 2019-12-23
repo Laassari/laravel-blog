2d89905106b2c6736e6bb00bfcda7d845b13dca3
Laassari <nlaassari@gmail.com>
2019-12-23 20:10:26 +0100

    test likeable existance in DB

--- tests/Feature/LikeableTest.php
+++ tests/Feature/LikeableTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Comment;
 use App\Post;
 use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -23,7 +24,23 @@ class LikeableTest extends TestCase
 
         $post->toggleLike();
 
+        $this->assertDatabaseHas('likes', [
+            'likeable_id' => $post->id,
+            'user_id' => $user->id,
+            'likeable_type' => Post::class,
+        ]);
+
         $this->assertEquals(1, $post->likes()->count());
+
+        $post->toggleLike();
+
+        $this->assertEquals(0, $post->likes()->count());
+
+        $this->assertDatabaseMissing('likes', [
+            'likeable_id' => $post->id,
+            'user_id' => $user->id,
+            'likeable_type' => Post::class,
+        ]);
     }
 
     /** @test */
@@ -42,6 +59,12 @@ class LikeableTest extends TestCase
             ->assertEquals(0, $comment->likes()->count());
 
         $comment->toggleLike();
+
+        $this->assertDatabaseHas('likes', [
+            'likeable_id' => $comment->id,
+            'user_id' => $user->id,
+            'likeable_type' => Comment::class,
+        ]);
 
         $this->assertEquals(1, $comment->likes()->count());
 
